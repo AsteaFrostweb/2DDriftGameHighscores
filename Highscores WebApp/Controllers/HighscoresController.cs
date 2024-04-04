7b21d9fa4f3362f42a8e8a618d6fc3e92b014012
Morgan Mulholland <morgan.p.mulholland@gmail.com>
2024-04-04 17:58:00 +0100

    Refactored Highscores controller GetHighscore function to include ToRank and UserName parameters. These are used to determine which highscores to send and if the UserName is specified it returns the users highscore for that map

--- Highscores WebApp/Controllers/HighscoresController.cs
+++ Highscores WebApp/Controllers/HighscoresController.cs
@@ -1,6 +1,7 @@
 ﻿using Highscores_WebApp.Data;
 using Highscores_WebApp.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
@@ -60,35 +61,84 @@ public class HighscoresController : Controller
 
     [HttpGet]
     [AllowAnonymous]
-    public IActionResult GetHighScores(string MapName)
+    public IActionResult GetHighScores(string MapName, int ToRank, string UserName)
     {
-        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-
-
-        var map = _context.Maps.FirstOrDefault(m => m.Name == MapName);
-        int map_id;
-        if (map == null)
+        
+        
+        //Validate inputs
+        if (ToRank <= 0) 
         {
-            return BadRequest("Map information is missing.");
+            return BadRequest("Must request more than 0 higshcores");
+        }
+        if (MapName == null) 
+        {
+            return BadRequest("Map name is required.");          
+        }       
+
+        //Finding the map from name to get the ID and to check it exists
+        MapsModel map_model = _context.Maps.FirstOrDefault(m => m.Name == MapName);
+        if (map_model == null)
+        {
+            return BadRequest("Unable to find map with the name specified");
+        }
+        int map_Id = map_model.Id;
+
+        var HighScoresQuery = _context.Highscores.Where(h => h.Map_Id == map_Id);
+        if (HighScoresQuery.Count<HighscoresModel>() == 0) 
+        {
+            return BadRequest("Unable to find any highsores for the map specified");
+        }
+
+        var users = _context.Users;
+
+        //if the request is for all users
+        if (UserName == null)
+        {           
+            List<HighscoresModel> fastest_combos = HighScoresQuery.OrderBy(h => h.Fastest_Lap).Take(ToRank).ToList();
+            List<HighscoresModel> longest_combos = HighScoresQuery.OrderByDescending(h => h.Best_Combo_Time).Take(ToRank).ToList();
+            List<HighscoresModel> best_combo_scores = HighScoresQuery.OrderByDescending(h => h.Best_Combo_Score).Take(ToRank).ToList();
+
+            List<HighscoresModel> highscores = new List<HighscoresModel>();
+            for (int i = 0; i < ToRank; i++)
+            {
+                if (!highscores.Contains(fastest_combos[i])) highscores.Add(fastest_combos[i]);
+                if (!highscores.Contains(longest_combos[i])) highscores.Add(longest_combos[i]);
+                if (!highscores.Contains(best_combo_scores[i])) highscores.Add(best_combo_scores[i]);
+            }
+
+            List<HighscoreGetModel> get_highscores = new List<HighscoreGetModel>();
+            foreach (var highscore in highscores)
+            {
+                IdentityUser? user = users.SingleOrDefault(u => u.Id == highscore.Player_Id);
+                if (user != null)
+                {
+                    get_highscores.Add(highscore.AsGetModel(MapName, user.UserName));
+                }
+            }
+
+            return Ok(get_highscores);
         }
         else
         {
-            map_id = map.Id;
-        }            
-        
-       
-        var HighScoresQuery = _context.Highscores.Where(h => h.Map_Id == map_id);
-        
+            IdentityUser user_model = _context.Users.FirstOrDefault(u => u.UserName == UserName);
+            if (user_model == null)
+            {
+                return BadRequest("Unable to find user with the name specified");
+            }
+            string user_Id = user_model.Id;
 
-        var HighScores = HighScoresQuery.ToList();
+            //if the request is for only a single user
+            HighscoresModel? HighScore = HighScoresQuery.FirstOrDefault(h => h.Map_Id == map_Id && h.Player_Id == user_Id);
+            if (HighScore == null) 
+            {
+                return BadRequest("Failed to find player highscores");
+            }
 
-        List<HighscoreGetModel> returnhighscores = new List<HighscoreGetModel>();
-        foreach (var highscore in HighScoresQuery)
-        {
-            returnhighscores.Add(highscore.AsGetModel(_context.Maps.FirstOrDefault(m => m.Id == highscore.Map_Id).Name, _context.Users.FirstOrDefault(u => u.Id == highscore.Player_Id).UserName));
+            List<HighscoreGetModel> get_highscores = new List<HighscoreGetModel>();
+            get_highscores.Add(HighScore.AsGetModel(MapName, UserName));
+            return Ok(get_highscores);
         }
 
-        return Ok(returnhighscores);
     }
 
     [Authorize]
